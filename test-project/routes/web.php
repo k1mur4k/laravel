b10b6c851110d154152f234ae74031a2007bfe17
kagaya <kagayatest1@gmail.com>
2024-05-24 17:22:10 +0900

    最新版

--- test-project/routes/web.php
+++ test-project/routes/web.php
@@ -2,6 +2,31 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ManagementUserController;
+
+// 管理者ページのルート
+Route::get('/admin.index', [ManagementUserController::class, 'index'])
+            ->name('admin.index');
+
+// ユーザーの編集ページ
+Route::get('/user/{id}/edit', [ManagementUserController::class, 'edit'])
+            ->name('user.edit');
+
+// ユーザーの更新
+Route::put('/user/{id}', [ManagementUserController::class, 'update'])
+    ->name('user.update');
+
+// ユーザーの削除
+Route::delete('/user/{id}/delete', [ManagementUserController::class, 'delete'])
+            ->name('user.delete');
+
+// ユーザーの論理削除
+Route::delete('/user/{id}/soft-delete', [ManagementUserController::class, 'softDelete'])
+    ->name('user.soft-delete');
+
+// ユーザ表示テスト
+// Route::get('/unko', [ManagementUserController::class, 'test'])
+//             ->name('unko');
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +41,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// Route::get('/management', function (){
+//     return view('management');
+// })
 
 require __DIR__.'/auth.php';
